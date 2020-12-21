a3a6232648b81836a33d78263442f7fe975b25a4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-12-21 17:15:53 +0100

    Action skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const query: string = core.getInput('query')
+    const variables: string = core.getInput('variables')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Query: ${query}`)
+    core.debug(`Variables: ${variables}`)
   } catch (error) {
     core.setFailed(error.message)
   }
