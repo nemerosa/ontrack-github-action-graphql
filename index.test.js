2bf8a59d86296e764ded7d3cfef9b72093b1d897
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-12-22 15:56:39 +0100

    Testing the GraphQL layer with variables

--- index.test.js
+++ index.test.js
@@ -6,23 +6,57 @@ const PROJECT_LIST = `{
     }
 }`;
 
-test('Ontrack URL is required', async () => {
-    process.env['ONTRACK_URL'] = '';
-    process.env['ONTRACK_TOKEN'] = '';
-    await expect(graphql(PROJECT_LIST, {})).rejects.toThrow('ONTRACK_URL environment variable is required.');
-});
+const PROJECT_BY_NAME = `query ProjectByName($name: String!) {
+    projects(name: $name) {
+        name
+    }
+}`;
 
-test('Ontrack token is required', async () => {
-    process.env['ONTRACK_URL'] = 'http://localhost:8080';
-    process.env['ONTRACK_TOKEN'] = '';
-    await expect(graphql(PROJECT_LIST, {})).rejects.toThrow('ONTRACK_TOKEN environment variable is required.');
+describe('With missing configuration', () => {
+
+    let oldUrl = '';
+    let oldToken = '';
+
+    beforeEach(() => {
+        oldUrl = process.env['ONTRACK_URL'];
+        oldToken = process.env['ONTRACK_TOKEN'];
+    });
+
+    afterEach(() => {
+        process.env['ONTRACK_URL'] = oldUrl;
+        process.env['ONTRACK_TOKEN'] = oldToken;
+    });
+
+    test('Ontrack URL is required', async () => {
+        process.env['ONTRACK_URL'] = '';
+        process.env['ONTRACK_TOKEN'] = '';
+        await expect(graphql(PROJECT_LIST, {})).rejects.toThrow('ONTRACK_URL environment variable is required.');
+    });
+
+    test('Ontrack token is required', async () => {
+        process.env['ONTRACK_URL'] = 'http://localhost:8080';
+        process.env['ONTRACK_TOKEN'] = '';
+        await expect(graphql(PROJECT_LIST, {})).rejects.toThrow('ONTRACK_TOKEN environment variable is required.');
+    });
+
 });
 
 describe('Ontrack is configured', () => {
 
     test('Getting list of projects', async () => {
-        const data = await expect(graphql(PROJECT_LIST, {})).resolves.toBeDefined();
-        console.log("data = ", data);
+        await expect(graphql(PROJECT_LIST, {})).resolves.toSatisfy((data) => {
+            console.log("data = ", data);
+            return data.projects != null;
+        });
+    });
+
+    test('Getting a project by name', async () => {
+        await expect(graphql(PROJECT_BY_NAME, {name: 'ontrack'})).resolves.toSatisfy((data) => {
+            const projects = data.projects;
+            expect(projects.length).toEqual(1)
+            const project = projects[0];
+            return project.name === 'ontrack';
+        });
     });
 
 });
